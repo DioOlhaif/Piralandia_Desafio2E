308b6c991a0b34c81f94a8d1dbb1cb96e4ff32dc
Dio <diovane.fialho@sga.pucminas.br>
2021-03-16 01:09:09 -0300

    Adicionar arquivos de projeto.

---
+++ Piralandia_Desafio2E/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Piralandia_Desafio2E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // METODO 1
+
+            string n = Console.ReadLine();
+
+            char[] arr = n.ToCharArray();
+
+            Array.Reverse(arr);
+
+
+
+            string v = new string(arr);
+
+            Console.WriteLine(v);
+
+
+
+
+            // METODO 2
+
+            static void Main(string[] args) { 
+            string n = Console.ReadLine();
+            
+
+            
+
+            
+            
+            string v = new string(n.Reverse().ToArray());
+            Console.WriteLine(v);
+
+
+        
+
+
+        }
+    }
+}
